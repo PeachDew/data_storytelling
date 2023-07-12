afc76133206ff9f00fe85ffbc3427e86e3c850fc
PeachDew <chin.furui.raymond@dhs.sg>
2023-07-13 02:50:11 +0800

    Working on streamlit pages

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -32,16 +32,21 @@ for _ in range(99):
 
 # Add one outlier 
 data['Year'].append(2022)
-data['Sales'].append(10000000)  # Outlier value
-data['Expenses'].append(500000)
-data['Profit'].append(9500000)
-data['Region'].append('Outlier')
+sales = 98020042
+expenses = random.randint(5000, 100000)
+profit = sales - expenses
+data['Sales'].append(sales)  
+data['Expenses'].append(expenses)
+data['Profit'].append(profit)
+data['Region'].append(random.choice(['North', 'South', 'East', 'West']))
 
 df = pd.DataFrame(data)
 
 st.title("Storytelling with Data 🎭📈")
 
-st.dataframe(df, hide_index=True)
+with st.columns(3)[1]:
+    st.dataframe(df, hide_index=True)
+# or st.columns(3)[1].dataframe(df)    
 
 st.markdown('''
 ## Using the numbers themselves
@@ -54,5 +59,5 @@ percentage_difference = ((max_sale - mean_sale) / mean_sale) * 100
 max_sales_year = df.loc[df['Sales'].idxmax(), 'Year']
 
 st.markdown(f'''
-In {max_sales_year} we had {max_sale} sakes, which was {percentage_difference:.2f}% larger than on average!
+In {max_sales_year} we had {max_sale} sales, which was {percentage_difference:.2f}% larger than on average!
 ''')
