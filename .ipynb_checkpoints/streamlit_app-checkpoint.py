806beafd1f01659046c51d3da037896f3c39a2d8
PeachDew <chin.furui.raymond@dhs.sg>
2023-07-14 01:13:29 +0800

    Working on streamlit pages

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -85,6 +85,7 @@ plt.style.use('dark_background')
 fig, ax = plt.subplots()
 ax = sns.heatmap(df, cmap='YlGnBu', annot=True, fmt='.0f', cbar=True)
 ax.patch.set_alpha(0.7)
+fig.patch.set_alpha(0.7)
 plt.title('Sales Performance by Year and Region')
 ax.set_yticklabels(ax.get_yticklabels(), rotation=45, ha='right')
 
