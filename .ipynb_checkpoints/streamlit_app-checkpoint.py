a5ae33f68bcff46f6d4a96d959b6e260472f5c56
PeachDew <chin.furui.raymond@dhs.sg>
2023-07-14 00:41:36 +0800

    Working on streamlit pages

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -44,7 +44,7 @@ df = pd.DataFrame(data)
 
 st.title("Storytelling with Data 🎭📈")
 
-with st.columns([2,8,1])[1]:
+with st.columns([2.5,8,1])[1]:
     st.dataframe(df, hide_index=True)
 # or st.columns(3)[1].dataframe(df)    
 
