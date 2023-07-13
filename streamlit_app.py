f961632de745577bae63b576fd94ea134a185738
PeachDew <chin.furui.raymond@dhs.sg>
2023-07-14 01:10:59 +0800

    Working on streamlit pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,9 +84,9 @@ plt.style.use('dark_background')
 
 fig, ax = plt.subplots()
 ax = sns.heatmap(df, cmap='YlGnBu', annot=True, fmt='.0f', cbar=True)
+ax.patch.set_alpha(0.7)
 plt.title('Sales Performance by Year and Region')
 ax.set_yticklabels(ax.get_yticklabels(), rotation=45, ha='right')
-ax.patch.set_alpha(0.5)
 
 st.pyplot(fig)
 
