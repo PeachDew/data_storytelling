e101eb1260ebc7f898eb8c70acfce742aa7417b0
PeachDew <chin.furui.raymond@dhs.sg>
2023-07-14 16:38:39 +0800

    Working on streamlit pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -219,7 +219,7 @@ st.divider()
 st.markdown("### Closure ⭕️")
 colcl1, colcl2 = st.columns(2)
 with colcl1:
-    st.markdown('''The closure concept suggests that our minds prefer simplicity and tend to fill in missing parts to make sense of incomplete shapes. Our eyes automatically complete the picture, allowing us to perceive a whole even when some elements are missing.
+    st.markdown('''Our minds prefer simplicity and tend to fill in missing parts to make sense of incomplete shapes. We automatically complete the picture, allowing us to perceive a whole even when some elements are missing.
              
 Removing unnecessary elements like chart borders and background shading, as guided by the closure principle, allows our graph to appear cohesive. Furthermore, this helps emphasize our data.''')
 with colcl2:
@@ -236,11 +236,14 @@ with colcl2:
     axes[1].barh(np.arange(len(categories)), values, edgecolor='none')
     axes[1].set_yticks(np.arange(len(categories)))
     axes[1].set_yticklabels(categories)
+    axes[0].set_title('Bar Chart Simplified')
 
     axes[1].spines['left'].set_visible(False)
     axes[1].spines['bottom'].set_visible(False)
     axes[1].spines['right'].set_visible(False)
     axes[1].spines['top'].set_visible(False)
+    
+    plt.subplots_adjust(wspace=0.3)
 
     st.pyplot(fig)
 st.divider()
