22fce4fcb86fb8cee2cf610e9cde9f10dc756b21
PeachDew <chin.furui.raymond@dhs.sg>
2023-07-19 19:37:23 +0800

    Working on streamlit pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -568,14 +568,14 @@ def color(x):
 def bold(x):
     return f"font-weight: bold; font-size: 32px;" if  x == 'A' else None
 
-
-styled = df.style.applymap(color, subset=("Hue","Column3"))
-styled2 = styled.applymap(bold, subset=('Intensity', "Column3"))
-styled2 = styled2.style.set_table_styles([
+df.style.set_table_styles([
     {'selector': 'thead', 'props': [('display', 'none')]}
 ])
+styled = df.style.applymap(color, subset=("Hue","Column3"))
+styled2 = styled.applymap(bold, subset=('Intensity', "Column3"))
 
-st.dataframe(styled2)
+
+st.dataframe(styled2, height = 100)
 
 
 
